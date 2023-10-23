bad619c35cafb18329c2dd0820f06e8573af62c8
epicary22 <epicary22@gmail.com>
2023-10-23 18:42:07 +0000

    update PletlangChar to have a better data structure, PletlangCharHelper likewise

--- pletlang_char.py
+++ pletlang_char.py
@@ -5,20 +5,20 @@ class PletlangChar(Enum):
     """
     All of the 16 possible Pletlang characters, in enum form.
     """
-    END = {"int": 0b0000, "str": "]"}
-    D = {"int": 0b0001, "str": "d"}
-    M = {"int": 0b0010, "str": "m"}
-    N = {"int": 0b0011, "str": "n"}
-    V = {"int": 0b0100, "str": "v"}
-    W = {"int": 0b0101, "str": "w"}
-    Z = {"int": 0b0110, "str": "z"}
-    J = {"int": 0b0111, "str": "j"}
-    A = {"int": 0b1000, "str": "a"}
-    I = {"int": 0b1001, "str": "i"}
-    O = {"int": 0b1010, "str": "o"}
-    U = {"int": 0b1011, "str": "u"}
-    LO = {"int": 0b1100, "str": ","}
-    HI = {"int": 0b1101, "str": "'"}
-    BACKSLASH = {"int": 0b1110, "str": "\\"}
-    START = {"int": 0b1111, "str": "["}
+    END = 0b0000
+    D = 0b0001
+    M = 0b0010
+    N = 0b0011
+    V = 0b0100
+    W = 0b0101
+    Z = 0b0110
+    J = 0b0111
+    A = 0b1000
+    I = 0b1001
+    O = 0b1010
+    U = 0b1011
+    LO = 0b1100
+    HI = 0b1101
+    BACKSLASH = 0b1110
+    START = 0b1111
     # TODO change up this data structure? It seems a little backwards...
--- pletlang_char_helper.py
+++ pletlang_char_helper.py
@@ -2,8 +2,8 @@ from pletlang_char import PletlangChar
 
 
 class PletlangCharHelper:
-    VALID_INTS = [char.value["int"] for char in PletlangChar]
-    VALID_CHARS = [char.value["str"] for char in PletlangChar]
+	VALID_INTS = [char.value for char in PletlangChar]
+    VALID_CHARS = [']', 'd', 'm', 'n', 'v', 'w', 'z', 'j', 'a', 'i', 'o', 'u', ',', '\'', '\\', '[']
 
     @staticmethod
     def to_pletlang_char(obj: int | str | None):
@@ -21,30 +21,45 @@ class PletlangCharHelper:
         return PletlangChar.A
 
     @staticmethod
-    def __to_interpretable_char(string: str) -> str:
+    def __to_interpretable_char(string: str) -> str | None:
         """
         Turns any str into a Pletlang-interpretable character.\n
+		Returns None if the first character of string is ' ', or string is ''.\n
         :param string: Any str.
         :return: A character that can be interpreted into a PletlangChar.
         """
         if len(string) == 0:
-            return ' '
+            return None
         char_to_check = string[0]
-        if char_to_check not in [PletlangChar.VALID_STRS]:
-            return ' '
+        if char_to_check not in [VALID_CHARS]:
+            return None
         return char_to_check
 
+	@staticmethod
+	def __to_interpretable_int(string: str) -> int | None:
+		"""
+		Turns any str into a Pletlang-interpretable int.\n
+		Returns None if the first character of string is ' ', or string is ''.\n
+		:param string: Any str.
+		:return: An int that can be interpreted into a PletlangChar.
+		"""
+		char = PletlangCharHelper.__to_interpretable_char(string)
+		if char is None:
+			return None
+		return VALID_CHARS.index(char)
+
     @staticmethod
-    def to_pletlang_char_from_char(char: str) -> None | PletlangChar:
+    def to_pletlang_char_from_char(char: str) -> PletlangChar | None:
         """
         Finds the PletlangChar representation of a singular character.\n
         If a multi-character str is provided, only the first character is interpreted.\n
         :param char: The character to be interpreted into Pletlang.
         :return: The PletlangChar representation of the single character.
-        """
+		"""
         char = PletlangCharHelper.__to_interpretable_char(char)
         if char == ' ':
             return None
         else:
             char_index = PletlangCharHelper.VALID_CHARS.index(char)
             # TODO aaaaaaa I need to change the data structure of PletlangChar
+
