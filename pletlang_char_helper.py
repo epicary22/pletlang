d5313e981ec389bb8af83c29bc832cb7eeb7687c
epicary22 <epicary22@gmail.com>
2023-10-23 21:24:00 -0500

    fix tabs and spaces being mixed

--- pletlang_char_helper.py
+++ pletlang_char_helper.py
@@ -2,7 +2,7 @@ from pletlang_char import PletlangChar
 
 
 class PletlangCharHelper:
-	VALID_INTS = [char.value for char in PletlangChar]
+    VALID_INTS = [char.value for char in PletlangChar]
     VALID_CHARS = [']', 'd', 'm', 'n', 'v', 'w', 'z', 'j', 'a', 'i', 'o', 'u', ',', '\'', '\\', '[']
 
     @staticmethod
@@ -24,29 +24,29 @@ class PletlangCharHelper:
     def __to_interpretable_char(string: str) -> str | None:
         """
         Turns any str into a Pletlang-interpretable character.\n
-		Returns None if the first character of string is ' ', or string is ''.\n
+        Returns None if the first character of string is ' ', or string is ''.\n
         :param string: Any str.
         :return: A character that can be interpreted into a PletlangChar.
         """
         if len(string) == 0:
             return None
         char_to_check = string[0]
-        if char_to_check not in [VALID_CHARS]:
+        if char_to_check not in [PletlangCharHelper.VALID_CHARS]:
             return None
         return char_to_check
 
-	@staticmethod
-	def __to_interpretable_int(string: str) -> int | None:
-		"""
-		Turns any str into a Pletlang-interpretable int.\n
-		Returns None if the first character of string is ' ', or string is ''.\n
-		:param string: Any str.
-		:return: An int that can be interpreted into a PletlangChar.
-		"""
-		char = PletlangCharHelper.__to_interpretable_char(string)
-		if char is None:
-			return None
-		return VALID_CHARS.index(char)
+    @staticmethod
+    def __to_interpretable_int(string: str) -> int | None:
+        """
+        Turns any str into a Pletlang-interpretable int.\n
+        Returns None if the first character of string is ' ', or string is ''.\n
+        :param string: Any str.
+        :return: An int that can be interpreted into a PletlangChar.
+        """
+        char = PletlangCharHelper.__to_interpretable_char(string)
+        if char is None:
+            return None
+        return PletlangCharHelper.VALID_CHARS.index(char)
 
     @staticmethod
     def to_pletlang_char_from_char(char: str) -> PletlangChar | None:
@@ -55,11 +55,10 @@ class PletlangCharHelper:
         If a multi-character str is provided, only the first character is interpreted.\n
         :param char: The character to be interpreted into Pletlang.
         :return: The PletlangChar representation of the single character.
-		"""
+    """
         char = PletlangCharHelper.__to_interpretable_char(char)
         if char == ' ':
             return None
         else:
             char_index = PletlangCharHelper.VALID_CHARS.index(char)
             # TODO aaaaaaa I need to change the data structure of PletlangChar
-
