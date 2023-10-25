4b96e4bdade57d7217d04427152c791d836aede3
epicary22 <epicary22@gmail.com>
2023-10-25 14:41:57 +0000

    Make PletlangCharHelper use typing.Union rather than | operator

--- pletlang_char_helper.py
+++ pletlang_char_helper.py
@@ -1,12 +1,17 @@
+from typing import Union
 from pletlang_char import PletlangChar
 
 
 class PletlangCharHelper:
+    """
+    Helper methods and values for the PletlangChar enum.\n
+    Any operations for PletlangChars can be found here.
+    """
     VALID_INTS = [char.value for char in PletlangChar]
     VALID_CHARS = [']', 'd', 'm', 'n', 'v', 'w', 'z', 'j', 'a', 'i', 'o', 'u', ',', '\'', '\\', '[']
 
     @staticmethod
-    def to_pletlang_char(obj: int | str | None = None):
+    def to_pletlang_char(obj: Union[int, str, None] = None) -> Union[PletlangChar, None]:
         """
         Finds the Pletlang character representation of a given int, str, or None.\n
         ints between 0 and 15 are valid. ints < 0 are interpreted as 0. ints > 15 are interpreted as 15.\n
@@ -23,7 +28,7 @@ class PletlangCharHelper:
         return None
 
     @staticmethod
-    def __to_interpretable_char(string: str) -> str | None:
+    def __to_interpretable_char(string: str) -> Union[str, None]:
         """
         Turns any str into a Pletlang-interpretable character.\n
         Returns None if the first character of string is ' ', or string is ''.\n
@@ -38,7 +43,7 @@ class PletlangCharHelper:
         return char_to_check
 
     @staticmethod
-    def __to_interpretable_int(string: str) -> int | None:
+    def __to_interpretable_int(string: str) -> Union[int, None]:
         """
         Turns any str into a Pletlang-interpretable int.\n
         Returns None if the first character of string is ' ', or string is ''.\n
@@ -51,13 +56,13 @@ class PletlangCharHelper:
         return PletlangCharHelper.VALID_CHARS.index(char)
 
     @staticmethod
-    def to_pletlang_char_from_char(char: str) -> PletlangChar | None:
+    def to_pletlang_char_from_char(char: str) -> Union[PletlangChar, None]:
         """
         Finds the PletlangChar representation of a singular character.\n
         If a multi-character str is provided, only the first character is interpreted.\n
         :param char: The character to be interpreted into Pletlang.
         :return: The PletlangChar representation of the single character.
-    """
+        """
         pletlang_char_index = PletlangCharHelper.__to_interpretable_int(char)
         if pletlang_char_index is None:
             return None
@@ -65,7 +70,14 @@ class PletlangCharHelper:
             return PletlangCharHelper.to_pletlang_char_from_int(pletlang_char_index)
 
     @staticmethod
-    def to_pletlang_char_from_int(pletlang_char_index: int) -> PletlangChar | None:
+    def to_pletlang_char_from_int(pletlang_char_index: int) -> Union[PletlangChar, None]:
+        """
+        Finds the PletlangChar representation of a single int.\n
+        ints provided between 0 and 15 will return proper PletlangChar values, and anything
+        else will return None.\n
+        :param pletlang_char_index: The int to be interpreted as a PletlangChar.
+        :return: The PletlangChar interpretation, if one exists.
+        """
         if pletlang_char_index in PletlangCharHelper.VALID_INTS:
             return [char for char in PletlangChar][pletlang_char_index]
         return None
