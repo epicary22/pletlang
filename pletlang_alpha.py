e61923dc180be0e441c43b1af50a0f70376bb8c6
epicary22 <epicary22@gmail.com>
2023-10-28 17:19:35 -0500

    begin PletlangAlpha

---
+++ pletlang_alpha.py
@@ -0,0 +1,8 @@
+from pletlang_char import PletlangChar
+
+
+class PletlangAlpha:
+	"""
+	Methods and values relating to translating Pletlang into or from human-readable text.
+	"""
+	VALUES = [']', 'd', 'm', 'n', 'v', 'w', 'z', 'j', 'a', 'i', 'o', 'u', ',', '\'', '\\', '[']
