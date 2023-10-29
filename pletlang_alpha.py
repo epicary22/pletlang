52893bd5a0a8b894f9bd8c5a0a59d7b958953ca5
epicary22 <epicary22@gmail.com>
2023-10-28 20:29:06 -0500

    add more PletlangAlpha methods

--- pletlang_alpha.py
+++ pletlang_alpha.py
@@ -1,4 +1,6 @@
 from pletlang_char import PletlangChar
+from pletlang_char_helper import PletlangCharHelper
+from typing import Union
 
 
 class PletlangAlpha:
@@ -6,3 +8,23 @@ class PletlangAlpha:
 	Methods and values relating to translating Pletlang into or from human-readable text.
 	"""
 	VALUES = [']', 'd', 'm', 'n', 'v', 'w', 'z', 'j', 'a', 'i', 'o', 'u', ',', '\'', '\\', '[']
+	
+	@staticmethod
+	def from_pletlang_char(pletlang_char: PletlangChar) -> str:
+		"""
+		
+		:param pletlang_char:
+		:return:
+		"""
+		return PletlangAlpha.VALUES[pletlang_char.value]
+	
+	@staticmethod
+	def to_pletlang_char(alpha_char: str) -> Union[PletlangChar, None]:
+		"""
+		
+		:param alpha_char:
+		:return:
+		"""
+		if alpha_char in PletlangAlpha.VALUES:
+			return PletlangCharHelper.VALUES[PletlangAlpha.VALUES.index(alpha_char)]
+		return None
