ae21000fc66da17c53659c92dec9e4b51acc2db9
epicary22 <epicary22@gmail.com>
2023-12-11 10:36:32 -0600

    implement basic PletlangChar in java, change the file structure

---
+++ java/pletlang/src/composition/TestClass.java
@@ -0,0 +1,10 @@
+package composition;
+
+public class TestClass
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Wow this is a test class!");
+        System.out.println(PletlangChar.A.ordinal());
+    }
+}
