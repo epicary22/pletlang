f8352d09ef3c3fa2931396f4f141150ee4fd6609
epicary22 <epicary22@gmail.com>
2023-12-11 17:21:32 -0600

    Fix up the directory structure again, looks like it's ready to go now.

--- java/pletlang/src/composition/TestClass.java
+++ java/pletlang/src/composition/TestClass.java
@@ -5,6 +5,6 @@ public class TestClass
     public static void main(String[] args)
     {
         System.out.println("Wow this is a test class!");
-        System.out.println(PletlangChar.A.ordinal());
+        System.out.println(PletlangChar.A.getValue());
     }
 }
