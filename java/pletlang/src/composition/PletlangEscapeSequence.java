52566026b2733226175eff95a5c0367d2237c395
epicary22 <epicary22@gmail.com>
2023-12-19 10:39:05 -0600

    Figure out that I want to make looking up PletASM functions a dictionary

--- java/pletlang/src/composition/PletlangEscapeSequence.java
+++ java/pletlang/src/composition/PletlangEscapeSequence.java
@@ -1,7 +1,11 @@
 package composition;
 
+// I can do this with JSON. Will be prototyping in Python.
+
 public class PletlangEscapeSequence
 {
+    public static dictionary = new TreeMap<>
+
     public PletlangEscapeSequenceType type;
 
     public PletlangEscapeSequence()
